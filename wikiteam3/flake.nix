c15e1fb9f7a1908fa27f05ba7c63dbdd680b64b8
Janet Cobb <git@randomcat.org>
2024-01-22 13:27:12 -0500

    [wikiteam3] Rewrite poetry

--- wikiteam3/flake.nix
+++ wikiteam3/flake.nix
@@ -1,75 +1,66 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  description = "Application packaged using poetry2nix";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs.wikiteam3 = {
-    url = "github:elsiehupp/wikiteam3";
-    flake = false;
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    wikiteam3 = {
+      url = "github:mediawiki-client-tools/mediawiki-dump-generator";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, wikiteam3 }:
-    let
-      appNames = [ "dumpgenerator" "launcher" "uploader" ];
-    in
-    {
-      # Nixpkgs overlay providing the application
-      overlay = nixpkgs.lib.composeManyExtensions [
-        (final: prev:
-          {
-            # The application
-            dumpgenerator =
-              let
-                basePackage = final.poetry2nix.mkPoetryApplication {
-                  projectDir = wikiteam3;
-
-                  overrides = final.poetry2nix.overrides.withDefaults (final: prev: {
-                    pre-commit-poetry-export = prev.pre-commit-poetry-export.overridePythonAttrs (old: {
-                      buildInputs = (old.buildInputs or []) ++ [final.poetry];
-                    });
-
-                    wikitools3 = prev.wikitools3.overridePythonAttrs (old: {
-                      buildInputs = (old.buildInputs or []) ++ [final.poetry];
-                    });
-                  });
-                };
-
-                wrappedPackage = prev.linkFarm "wikiteam3-wrapped" (
-                  map (app: {
-                    name = "bin/wikiteam-${app}";
-                    path = "${basePackage}/bin/${app}";
-                  }) appNames
-                );
-              in
-              wrappedPackage;
-          }
-        )
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, wikiteam3 }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-
-        lib = nixpkgs.lib;
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        instance = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        inherit (instance) mkPoetryApplication;
+        appNames = [ "dumpgenerator" "launcher" "uploader" ];
       in
-      rec {
+      {
         packages = {
-          dumpgenerator = pkgs.dumpgenerator;
-          default = packages.dumpgenerator;
+          unwrapped = mkPoetryApplication {
+            projectDir = "${wikiteam3}";
+
+            overrides = instance.overrides.withDefaults (final: prev: {
+              pre-commit-poetry-export = prev.pre-commit-poetry-export.override {
+                preferWheel = true;
+              };
+
+              wikitools3 = prev.wikitools3.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or []) ++ [final.poetry];
+              });
+            });
+          };
+
+          dumpgenerator = nixpkgs.legacyPackages.${system}.linkFarm "wikiteam3-wrapped" (
+            map (app: {
+              name = "bin/wikiteam-${app}";
+              path = "${self.packages.${system}.unwrapped}/bin/${app}";
+            }) appNames
+          );
+
+          default = self.packages.${system}.dumpgenerator;
         };
 
-        apps =
-          let
-            base = lib.genAttrs appNames (app: {
-              type = "app";
-              program = "${packages.dumpgenerator}/bin/wikiteam-${app}";
-            });
-          in
-          base // {
-            default = base.dumpgenerator;
-          };
-      }
-   ));
+        apps = nixpkgs.lib.genAttrs appNames (app: {
+          type = "app";
+          program = "${self.packages.${system}.dumpgenerator}/bin/wikiteam-${app}";
+        }) // {
+          default = self.apps.${system}.dumpgenerator;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.dumpgenerator ];
+          packages = [ pkgs.poetry ];
+        };
+      });
 }
